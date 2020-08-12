c3afe67e656daf6698d87c8cdc697d6f4a9eed2f
annia valdes <millerl.mb79@gmail.com>
2020-08-12 08:18:39 -0500

    add Vote model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User ,Vote } = require('../../models');
 
 
 
@@ -92,6 +92,18 @@ router.put('/:id', (req, res) => {
 });
 
 
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => res.json(err));
+});
+
+
+
 router.delete('/:id', (req, res) => {
   Post.destroy({
     where: {
@@ -110,6 +122,9 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
+
 
 
   module.exports = router;
