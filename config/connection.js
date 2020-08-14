7d0a19776e5936240a3d43cf6dcfdfe8f04d3e37
annia valdes <millerl.mb79@gmail.com>
2020-08-14 09:38:22 -0500

    add comments to the code

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
+//import the sequelize constructor from his lebrary
 const Sequelize=require('sequelize')
+require('dotenv').config();
 
+//create the connection to the local batabase
 let sequelize;
-
+// for heroku
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+   //for local database
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
