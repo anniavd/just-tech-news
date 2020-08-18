ae4bced3935895bd6383d79e4474c211ed571315
annia valdes <millerl.mb79@gmail.com>
2020-08-18 09:55:11 -0500

    add a view for do a comments.add a logic and update the router post

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,7 +3,7 @@ const { Comment } = require('../../models');
 
 
 
-
+//show all comments
 router.get('/', (req, res) => {
  Comment.findAll({})
  .then(dbGetData => res.json(dbGetData))
@@ -15,23 +15,27 @@ router.get('/', (req, res) => {
 
 
 
-
+//create a comments
 router.post('/', (req, res) => {
+  // check the session
+  if (req.session) {
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 
 
-
+//delete a comments 
 router.delete('/:id', (req, res) => {
     Comment.destroy({
         where: {
