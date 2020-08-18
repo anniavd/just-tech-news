ed1a17a56a9579fb363376e430801a36b3035a4d
annia valdes <millerl.mb79@gmail.com>
2020-08-17 19:39:11 -0500

    update server and routes for add session,can login and logout on the page.add html for the forms login and signup

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const path = require('path');
-// express-handlebars
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-
-
+//express conection
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
 
+// imports handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//creates session
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+//use the session
+app.use(session(sess));
+
+//middleware for express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// express-handlebars
+// handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
